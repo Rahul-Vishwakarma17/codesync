7d20b4a21a5834aa86643143f8197dd3381596c0
Rahul-Vishwakarma17 <rv832213@gmail.com>
2026-06-09 15:44:20 +0530

    Fix production socket connection

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,9 +1,14 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io(
+  import.meta.env.VITE_SOCKET_URL
+);
 
 socket.on("connect", () => {
-  console.log("Connected:", socket.id);
+  console.log(
+    "Connected:",
+    socket.id
+  );
 });
 
 export default socket;
